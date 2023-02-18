9584727d1006ba22e72abcbc448cc90b9e3a758f
ziedzayani <zayani.zied@gmail.com>
2023-02-18 17:52:57 +0100

    2 files modified resolve issue with stackblitz

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import 'zone.js/dist/zone';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
